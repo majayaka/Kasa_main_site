503400ab4ee12c4a2a2a28f83ede7dc0a9ab879c
majayaka <yumotoayaka@gmail.com>
2022-12-15 17:54:33 +0100

    changed the organisation of files

---
+++ src/App.js
@@ -0,0 +1,24 @@
+/* Import of React Router for... */
+import { Routes, Route } from 'react-router-dom';
+/* Import of pages */
+import Nomatch from './pages/Nomatch/Nomatch';
+import About from './pages/About/About';
+import Home from './pages/Home/Home';
+import SharedLayout from './pages/SharedLayout/SharedLayout';
+import Location from './pages/Location/Location';
+/* Import of data of apartments */
+import logements from './data/logements.json';
+
+
+export default function App () {
+  return (
+     <Routes> {/*Import of routes. The path and element is the returned component. */}
+      <Route path='/' element={<SharedLayout />}> {/* At the top of each route, we put "SharedLayout" including the header and the footer */}
+        <Route index element={<Home logements={logements}/>} />
+        <Route path='/location/:id' element={<Location /> } />
+        <Route path='about' element={<About />} />
+        <Route path='*' element={<Nomatch />} />
+      </Route>
+     </Routes>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import ReactDOM from 'react-dom/client';
 /* Import of React Router for... */
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
-/* Import of pages */
-import Nomatch from './pages/Nomatch/Nomatch';
-import About from './pages/About/About';
-import Home from './pages/Home/Home';
-import SharedLayout from './pages/SharedLayout/SharedLayout';
-import Location from './pages/Location/Location';
-/* Import of data of apartments */
-import logements from './data/logements.json';
+/* Import of App.js... */
+import App from './App';
 
 /* Render React to html with the div "root"
 , then put this snippet in a constant */
@@ -17,18 +11,13 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 /* generate the components with render method */
 <Router>
-     <Routes> {/*Import of routes. The path and element is the returned component. */}
-      <Route path='/' element={<SharedLayout />}> {/* At the top of each route, we put "SharedLayout" including the header and the footer */}
-        <Route index element={<Home logements={logements}/>} />
-        <Route path='/location/:id' element={<Location /> } />
-        <Route path='about' element={<About />} />
-        <Route path='*' element={<Nomatch />} />
-      </Route>
-     </Routes>   
+    <App />
 </Router>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
